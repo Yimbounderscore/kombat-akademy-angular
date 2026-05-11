d775630481777936b4907d0abacc78d1034a6fab
Ali Diksin <mrblueover9000@gmail.com>
2026-05-11 20:37:21 +0100

    1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,6 @@ import { Characters } from './pages/characters/characters';
 import { CharacterDetail } from './pages/character-detail/character-detail';
 import { Kameos } from './pages/kameos/kameos';
 import { KameoDetail } from './pages/kameo-detail/kameo-detail';
-import { MoveList } from './pages/move-list/move-list';
-import { Combos } from './pages/combos/combos';
 import { TierMaker } from './pages/tier-maker/tier-maker';
 import { Guides } from './pages/guides/guides';
 import { GuideTheMatch } from './pages/guides/the-match/the-match';
@@ -16,8 +14,6 @@ export const routes: Routes = [
   { path: 'character/:name', component: CharacterDetail },
   { path: 'kameos', component: Kameos },
   { path: 'kameos/:slug', component: KameoDetail },
-  { path: 'move-list', component: MoveList },
-  { path: 'combos', component: Combos },
   { path: 'tier-maker', component: TierMaker },
   { path: 'guides', component: Guides },
   { path: 'guides/the-match', component: GuideTheMatch },
