646368b3c2fab159ed80ea8ac46209a70256e10c
Ali Diksin <mrblueover9000@gmail.com>
2026-05-11 20:26:23 +0100

    placeholder

--- src/app/pages/characters/characters.ts
+++ src/app/pages/characters/characters.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { NgFor, NgIf } from '@angular/common';
+import { NgFor } from '@angular/common';
 import { CHARACTERS, Character } from '../../data/characters';
 
 @Component({
   selector: 'app-characters',
-  imports: [RouterLink, NgFor, NgIf],
+  imports: [RouterLink, NgFor],
   templateUrl: './characters.html',
   styleUrl: './characters.css',
 })
@@ -19,33 +19,14 @@ export class Characters implements OnInit {
   private loadCharacters(): void {
     const roster = [
       'Noob Saibot', 'Cyrax', 'Ermac', 'Kitana', 'Mileena', 'Tanya', 'Rain', 'Smoke', 'Scorpion', 'Sub-Zero',
-      'Reptile', 'Li Mei', 'Kenshi', 'Baraka', 'Geras', 'Omni-Man', 'Peacemaker', 'Ghostface', '', '',
-      'Sektor', 'Takeda', 'Shang Tsung', 'General Shao', 'Sindel', 'Reiko', 'Raiden', 'Liu Kang', '',
-      'Johnny Cage', 'Kung Lao', 'Ashrah', 'Nitara', 'Havik', 'Quan Chi', 'Homelander', '', ''
-    ];
+      'Reptile', 'Li Mei', 'Kenshi', 'Baraka', 'Geras', 'Omni-Man', 'Peacemaker', 'Ghostface',
+      'Sektor', 'Takeda', 'Shang Tsung', 'General Shao', 'Sindel', 'Reiko', 'Raiden', 'Liu Kang',
+      'Johnny Cage', 'Kung Lao', 'Ashrah', 'Nitara', 'Havik', 'Quan Chi', 'Homelander'
+    ]
+      .map(name => CHARACTERS[name])
+      .filter((character): character is Character => !!character);
 
-    const half = 19;
-    const row1: Character[] = [];
-    const row2: Character[] = [];
-
-    for (let i = 0; i < half; i++) {
-      const name = roster[i];
-      if (name) {
-        row1.push(CHARACTERS[name]);
-      } else {
-        row1.push(null as any);
-      }
-    }
-
-    for (let i = half; i < roster.length; i++) {
-      const name = roster[i];
-      if (name) {
-        row2.push(CHARACTERS[name]);
-      } else {
-        row2.push(null as any);
-      }
-    }
-
-    this.characterRows = [row1, row2];
+    const half = Math.ceil(roster.length / 2);
+    this.characterRows = [roster.slice(0, half), roster.slice(half)];
   }
 }
